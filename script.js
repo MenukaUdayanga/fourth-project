fd0789b6b4edc5371f391f31e9ac5f82cedcc986
MenukaUdayanga <ujayaweera738@gmail.com>
2023-06-04 22:48:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+  
+    // var email = "ujayaweera@gmail.com";
+    // alert(email);
+
+    var x = 10;
+    var y =15;
+
+    var answer = x+y;
+
+    console.log(answer);
+
+
+    // function
+
+    function add(){
+
+        var firstNumber = "Menuka";
+        var secondNumber = "Udayanga";
+
+        console.log("Answer is "+(firstNumber+" "+secondNumber));
+    
+
+    }
+
+    function userLogin(){
+      
+        var userName = document.getElementById("userName").value;
+        var password = document.getElementById("Password").value;
+
+       
+        document.getElementById("data").innerHTML="UserName is "+userName+" Password is "+password;
+    }
+
+ 
+
+   
